d4a8271c2e4c10e805ef90097e7eb94181c54112
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-12 23:12:15 +0530

    Added: Welcome message

---
+++ src/com/company/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Address Book Program");
+    }
+}
