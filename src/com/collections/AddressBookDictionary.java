7432c866a49ad878c992e323eeafbc677900630a
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-31 00:00:10 +0530

    Added: Code to check for duplicate entry within an address book

---
+++ src/com/collections/AddressBookDictionary.java
@@ -0,0 +1,12 @@
+package com.collections;
+
+import com.company.*;
+
+/**
+ * Address Book Dictionary creates key value pair to store contact data without creating duplicates in an address book
+ */
+public class AddressBookDictionary {
+    public static void main(String[] args){
+        AddressBookMain.addContactList();
+    }
+}
